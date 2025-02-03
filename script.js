4f58cd877b4bf38d65fc5168c048333c79820e0f
root <vitorsvierszc@gmail.com>
2025-02-03 15:53:42 -0300

    Cleaned up computer choice code Added player-choice prompt and standardized input

--- script.js
+++ script.js
@@ -3,13 +3,24 @@
 
 // computer choice
 
-
+//Improved computer random choice by outputting strings instead of console.log()
 
 function aiChoice(){
     let randomNum = Math.floor(Math.random()*3)
-    if(randomNum === 1){ console.log("Rock")}
-    else if (randomNum === 2){console.log("Paper")}
-    else{console.log("Scissors")}
+    if(randomNum == 1)
+        {return "rock"}
+    else if (randomNum == 2)
+        {return "paper"
+    }else
+        { return "scissors"}
 }
 
-aiChoice()
+console.log(aiChoice())
+
+//prompts player input and lowercases the string
+function playerChoice(){
+  let pChoice = prompt("Choose between Rock, Paper and Scissors" ).toLowerCase()
+  if(pChoice == "rock" || pChoice == "paper" || pChoice=="scissors") {return pChoice}
+  else {return "This answer isnt valid"} 
+}
+
